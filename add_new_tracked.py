bd8123c5907240fe17f46097fe1627f6c32db462
n-s405 <negi.samarth405@gmail.com>
2019-02-06 19:08:53 +0530

    Added gitignore and add_new_tracked.py for tracking priority users

---
+++ add_new_tracked.py
@@ -0,0 +1,14 @@
+import csv 
+
+print('-\n-\n-\n-\n-\n')
+
+print('Add new usernames to one of the following categories : "news" , "celebrities","politicians"')
+category = input('Enter the category\n') 
+
+
+if category == 'news':
+    pass
+elif category == 'celebrities' :
+    pass
+elif category == 'politicians':
+    pass
