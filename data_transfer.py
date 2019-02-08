083048285185e78b66f0d0b034cd8a2b3f978b9c
n-s405 <negi.samarth405@gmail.com>
2019-02-08 19:24:27 +0530

    Made the data_transfer module

--- data_transfer.py
+++ data_transfer.py
@@ -3,4 +3,73 @@ import csv
 
 
 
-def get_subject_keys
+def get_subject_keys(subject):
+    # Returns a list containing all the keys in a subject 
+
+    return_list = list()
+    if subject == 'c' :
+        target = f'data/celeb.csv'
+    elif subject == 'l':
+        target = f'data/leaders.csv'
+    elif subject == 'n':
+        target = f'data/news_outlet.csv'
+    elif subject == 'p':
+        target = f'data/parties.csv'
+    else:
+        target = f'data/{subject}.csv'
+    print(target)
+    with open(target) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count+=1 
+            else:
+                return_list.append(row[0])
+                line_count += 1
+    return return_list
+
+def get_list_of_subjects():
+    return 'celebs , leaders , news_outlets , parties'
+
+def write_to_csv(subject,data):
+    li = [data]
+    if subject == 'c' :
+        target = f'data/celeb.csv'
+    elif subject == 'l':
+        target = f'data/leaders.csv'
+    elif subject == 'n':
+        target = f'data/news_outlet.csv'
+    elif subject == 'p':
+        target = f'data/parties.csv'
+    else:
+        target = f'data/{subject}.csv'
+
+    print(f'write target is : {target}')
+
+    with open(target, mode='a') as open_file:
+        # employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        # employee_writer.writerow(li)
+        open_file.write(f'{data}\n')
+    return 0
+
+
+def add_key_to_subject(subject,addition):
+    lst = get_subject_keys(subject)
+    if addition in lst:
+        print('already present')
+        return 0 
+    else:
+        write_to_csv(subject , addition)
+
+
+
+
+
+def main():
+    pass
+    
+
+
+if __name__ == "__main__":
+    main()
